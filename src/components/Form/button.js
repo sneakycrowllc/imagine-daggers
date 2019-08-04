991ead38b47dbbc5a6b91e99e1e3468c6be8c86e
Zachary Sohovich <zachsohovich@thebriga.de>
2019-08-03 21:06:55 -0700

    created button component

---
+++ src/components/Form/button.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
+import { darken } from 'polished';
+
+const StyledButton = styled.button`
+  min-width: 100px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid ${props => props.theme.colors.CARDINAL};
+  padding: 8px;
+  border-radius: 2px;
+  font-weight: bold;
+  text-transform: uppercase;
+  font-size: 0.75em;
+  color: ${props => props.theme.colors.SNOW};
+  background-color: ${props => props.theme.colors.POMEGRANATE};
+  transition: background-color 100ms ease-in-out;
+  &:hover {
+    cursor: pointer;
+    background-color: ${props => props.theme.colors.CARDINAL};
+  }
+`
+
+const Button = (props) => {
+  return (
+    <StyledButton type={props.type}>{props.label}</StyledButton>
+  )
+}
+
+Button.propTypes = {
+  type: PropTypes.oneOf(['submit', 'button']),
+  label: PropTypes.string
+}
+
+Button.defaultProps = {
+  type: 'button',
+  label: 'Button'
+}
+
+export default Button;
