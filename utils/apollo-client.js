26a886f25873fa6de7a1bb697186b631d21ccaa3
Zachary E Sohovich <zach@sneakycrow.dev>
2019-12-04 21:21:44 -0800

    updated apollo client

--- utils/apollo-client.js
+++ utils/apollo-client.js
@@ -5,24 +5,33 @@ import { createHttpLink } from 'apollo-link-http';
 import { setContext } from 'apollo-link-context';
 import fetch from 'isomorphic-unfetch';
 
-const httpLink = createHttpLink({ 
-  fetch, 
-  uri: process.env.react_app_graphql_api
+const httpLink = createHttpLink({
+  fetch,
+  uri: process.env.graphql_api_url
 });
 
 const authLink = setContext((_, { headers }) => {
   return {
     headers: {
       ...headers,
-      'x-hasura-admin-secret': process.env.react_app_graphql_key
+      'x-hasura-admin-secret': process.env.graphql_api_key
     }
   };
+});
+
+export const graphQLClient = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache({
+    addTypename: false
+  })
 });
 
 export default withApollo(
   ({ initialState }) =>
     new ApolloClient({
       link: authLink.concat(httpLink),
-      cache: new InMemoryCache().restore(initialState || {})
+      cache: new InMemoryCache({
+        addTypename: false
+      }).restore(initialState || {})
     })
 );
