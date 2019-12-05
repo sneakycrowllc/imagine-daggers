e4ac5f7b5ca3aa54edbb11bbed19ce478356740b
Zachary E Sohovich <zach@sneakycrow.dev>
2019-12-04 21:21:37 -0800

    started register route

--- pages/api/register.js
+++ pages/api/register.js
@@ -1,10 +1,20 @@
 import bcrypt from 'bcrypt';
+import { graphQLClient } from '../../utils/apollo-client';
+import { NEW_USER_MUTATION } from '../../lib/mutations';
 
 // BCRYPT SETTINGS
 const saltRounds = 10;
 
 const registerHandler = (req, res) => {
-  res.json({ data: req.body });
-}
+  const { username, email, password } = req.body;
+  const newUser = {
+    username,
+    email,
+    password
+  };
+  graphQLClient
+    .mutate({ mutation: NEW_USER_MUTATION, variables: newUser })
+    .then(mutationResponse => res.json(mutationResponse));
+};
 
 export default registerHandler;
