5d62fd41420af4e379eb1bab695c23bc3a3a17b5
Zachary E Sohovich <zach@sneakycrow.dev>
2019-12-11 18:51:17 -0800

    added withAuth wrapper

--- utils/auth.js
+++ utils/auth.js
@@ -1,1 +1,15 @@
-// TODO: Add a wrapper for authenticating requests
+import jwt from 'jsonwebtoken';
+
+const withAuth = handler => (req, res) => {
+  const { authToken } = req.body;
+  jwt.verify(authToken, process.env.json_secret, (err) => {
+    if (err) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    return handler(req, res);
+  });
+};
+
+module.exports = {
+  withAuth
+};
