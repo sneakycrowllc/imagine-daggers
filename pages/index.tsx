e41dcd516b31f0ed1996f5756545451b2779f04f
Zachary E Sohovich <zach@sneakycrow.dev>
2020-01-18 14:29:16 -0800

    added input to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState, ChangeEvent } from 'react';
+
+import Input from '../components/input';
 
 const Home = () => {
+  const [username, setUsername] = useState<string>('');
+
   return (
     <div>
       <h1>Home</h1>
+      <Input
+        name="username"
+        label="Username"
+        value={username}
+        onChange={(event: ChangeEvent<HTMLInputElement>) => setUsername(event.target.value)}
+      />
     </div>
   );
 };
