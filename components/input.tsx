9da297e8ee8cf47431c5ed806e4e2bfee21dd0d5
Zachary E Sohovich <zach@sneakycrow.dev>
2020-01-18 18:04:07 -0800

    updated signup form logic

--- components/input.tsx
+++ components/input.tsx
@@ -1,5 +1,6 @@
-import React, { ChangeEvent } from 'react';
+import React, { ChangeEvent, useState, useRef } from 'react';
 import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 type TInputProps = {
   value: string;
@@ -7,15 +8,41 @@ type TInputProps = {
   label: string;
   name: string;
   placeholder?: string;
-  type?: string;
+  type?: 'text' | 'password';
 };
 
-const Input = ({ value, onChange, label, name, placeholder = '', type = 'text' }: TInputProps) => (
-  <StyledInput>
-    <label htmlFor={name}>{label}</label>
-    <input name={name} type={type} value={value} onChange={onChange} placeholder={placeholder} />
-  </StyledInput>
-);
+const Input = ({ value, onChange, label, name, placeholder = '', type = 'text' }: TInputProps) => {
+  // Dynamic input type for show/hide button
+  const [isPasswordShown, setPasswordShown] = useState<boolean>(false);
+  const inputEl = useRef<any>(null);
+  const handleShownPassword = (newToggleValue: boolean) => {
+    inputEl.current.focus();
+    setPasswordShown(newToggleValue);
+  };
+
+  return (
+    <StyledInput>
+      <label htmlFor={name}>{label}</label>
+      <StyledDynamicInput>
+        <input
+          name={name}
+          type={type === 'text' ? 'text' : isPasswordShown ? 'text' : 'password'} // eslint-disable-line
+          value={value}
+          onChange={onChange}
+          placeholder={placeholder}
+          ref={inputEl}
+        />
+        {type === 'password' && (
+          // @ts-ignore
+          <FontAwesomeIcon
+            icon={isPasswordShown ? 'eye-slash' : 'eye'}
+            onClick={() => handleShownPassword(!isPasswordShown)}
+          />
+        )}
+      </StyledDynamicInput>
+    </StyledInput>
+  );
+};
 
 const StyledInput = styled.div`
   min-width: 300px;
@@ -24,7 +51,6 @@ const StyledInput = styled.div`
   justify-content: flex-start;
   align-items: flex-start;
   padding: 8px;
-  border: 1px solid red;
   label {
     width: 100%;
     display: block;
@@ -32,8 +58,13 @@ const StyledInput = styled.div`
     font-size: 1em;
     margin-bottom: 4px;
   }
+`;
+
+const StyledDynamicInput = styled.div`
+  position: relative;
   input {
     width: 100%;
+    height: 50px;
     display: block;
     padding: 8px 16px;
     border: 2px solid #202020;
@@ -42,6 +73,13 @@ const StyledInput = styled.div`
       border-left-width: 6px;
       border-bottom-width: 6px;
     }
+  }
+  svg {
+    position: absolute;
+    right: 8px;
+    top: 0;
+    height: 100%;
+    font-size: 1.5em;
   }
 `;
 
