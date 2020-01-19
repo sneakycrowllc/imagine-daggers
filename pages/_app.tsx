9f1403c50d3c514ce423a202664b72d0edf6aac4
Zachary E Sohovich <zach@sneakycrow.dev>
2020-01-18 18:04:23 -0800

    added font awesome, updated logic for sign up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import App from 'next/app';
 import React from 'react';
 import { createGlobalStyle } from 'styled-components';
-
+import { config, library } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css'; // Import the CSS
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import ThemeToggler from '../components/themeToggler';
+
+library.add(faEye, faEyeSlash);
+
+config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 
 const GlobalStyle = createGlobalStyle`
   html {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,67 @@
-import React, { useState, ChangeEvent } from 'react';
+import React, { useState, ChangeEvent, FormEvent } from 'react';
+import styled from 'styled-components';
 
 import Input from '../components/input';
+import FormButton from '../components/formButton';
 
 const Home = () => {
   const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = {
+      username,
+      password,
+    };
+
+    fetch('/api/register', {
+      method: 'POST',
+      body: JSON.stringify(formData),
+    })
+      .then((res: any) => res.json())
+      .then(data => console.log(data));
+  };
 
   return (
     <div>
       <h1>Home</h1>
-      <Input
-        name="username"
-        label="Username"
-        value={username}
-        onChange={(event: ChangeEvent<HTMLInputElement>) => setUsername(event.target.value)}
-      />
+      <StyledRegisterSection>
+        <h2>Sign up</h2>
+        <StyledRegisterForm onSubmit={handleSubmit}>
+          <Input
+            name="username"
+            label="Username"
+            value={username}
+            onChange={(event: ChangeEvent<HTMLInputElement>) => setUsername(event.target.value)}
+            placeholder="agent_agentson93"
+          />
+          <Input
+            name="password"
+            label="Password"
+            value={password}
+            onChange={(event: ChangeEvent<HTMLInputElement>) => setPassword(event.target.value)}
+            type="password"
+          />
+          <FormButton>Submit</FormButton>
+        </StyledRegisterForm>
+      </StyledRegisterSection>
     </div>
   );
 };
+
+const StyledRegisterSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 24px;
+`;
+
+const StyledRegisterForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-end;
+`;
 
 export default Home;
